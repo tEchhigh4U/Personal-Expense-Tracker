772d87f2ee8957733675f1f6861d10e4dd3166f2
William Hui <williamhkw852@gmail.com>
2024-08-15 23:21:58 +0800

    ETM-5 add recentTransactionList component and Category extensions

--- ExpenseTrackerDemo/TransactionModel.swift
+++ ExpenseTrackerDemo/TransactionModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Transaction: Identifiable, Codable {
+struct Transaction: Identifiable, Codable, Hashable {
     let id: Int
     let date: String
     let institution: String
